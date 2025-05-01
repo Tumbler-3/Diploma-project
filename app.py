1ff0953e315f4dc53058567faaa8a257c05d78f1
Tamerlan <topkek164@gmail.com>
2025-05-02 02:15:24 +0600

    app upadate

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import streamlit as st
-import tensorflow as tf
 import numpy as np
 import cv2
 from keras import models
-
 
 class_names = ["Normal", "Diabetes", "Glaucoma", "Cataract",
                "Age related Macular Degeneration", "Hypertension",
@@ -15,11 +13,9 @@ try:
 except Exception as e:
     st.error(f"Error loading model: {e}")
     model = None  
-    
 
 st.title("Eye Disease Detection")
 uploaded_file = st.file_uploader("Upload an eye image", type=["jpg", "png", "jpeg"])
-
 
 if uploaded_file is not None and model is not None:
     file_bytes = np.asarray(bytearray(uploaded_file.read()), dtype=np.uint8)
@@ -29,15 +25,20 @@ if uploaded_file is not None and model is not None:
         st.error("Error loading the image.")
     else:
         image_resized = cv2.resize(image, (128, 128)) 
-        image_normalized = image_resized / 255.0 
-        image_input = np.expand_dims(image_normalized, axis=0) 
+        image_normalized = image_resized / 255.0  
+        image_input = np.expand_dims(image_normalized, axis=0)  
 
         st.image(image, caption="Uploaded Image", use_column_width=True)
 
         try:
             prediction = model.predict(image_input)
-            predicted_class = class_names[np.argmax(prediction)]  
+            print(f"Prediction: {prediction}")
+            print(f"Prediction shape: {prediction.shape}")
+
+            prediction = prediction.squeeze()  
+            predicted_class = class_names[np.argmax(prediction)]
             st.markdown(f"### Prediction: {predicted_class}")
+        
         except Exception as e:
             st.error(f"Error during prediction: {e}")
 else:
